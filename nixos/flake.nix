b25e2dfce7556793d915b95d449894dd7b05fc69
sytriz <reecerut@gmail.com>
2024-10-05 10:11:34 -0500

    first commit

---
+++ nixos/flake.nix
@@ -0,0 +1,31 @@
+{
+
+  description = "Sytriz's first flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+        inherit system;
+        modules = [ ./configuration.nix ];
+      };
+    };
+    homeConfigurations = {
+      sytriz = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];     
+      };
+    }; 
+  };
+
+}
